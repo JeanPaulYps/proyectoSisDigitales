93caa2febecb4240f8dce33774cd6f79c8095d65
Jean Paul <yjeanpaul@gmail.com>
2019-11-20 20:59:37 -0500

    Se agregaron las condiciones para ganar o perder el juego, se agrego una animacion

--- proyectoSisDigitales.ino
+++ proyectoSisDigitales.ino
@@ -3,7 +3,6 @@ int posicionSeleccionada = 0;
 int posicionPrevia = 0;
 
 int jugador = 1;
-boolean juegoTerminado = false;
 
 
 int botonSeleccion = 2;
@@ -60,15 +59,124 @@ void cambiarJugador ()
   else jugador = 1;
 }
 
+void encenderLEDS () {
+  for(int led = 0; led <= 8; led++) 
+  {
+    if (obtenerValorTablero(led) == 0)
+    {
+      digitalWrite(obtenerPosicionLED(led), LOW);
+    }
+    else if (obtenerValorTablero(led) == 1)
+    {
+      digitalWrite(obtenerPosicionLED(led), HIGH);
+    }
+    else if (obtenerValorTablero(led) == 2)
+    {
+      digitalWrite(obtenerPosicionLED(led), cicloJugador);
+    }
+    else if (obtenerValorTablero(led) == 3)
+    {
+      digitalWrite(obtenerPosicionLED(led), cicloSeleccion);
+    }
+    
+  }
+}
+
+void reiniciar ()
+{
+  for(int i = 0; i <= 8; i++) {
+    cambiarEstadoLED(i, LOW);
+    ponerValorTablero(i,0);
+  }
+  jugador = 0;
+  posicionSeleccionada = 0;
+  posicionPrevia = 0;
+
+}
+
+//Anumacion de ganar 
+void animacion (int led1, int led2, int led3)
+{
+  int i;
+  for(i = 0; i <= 8; i++) {
+    cambiarEstadoLED(i, LOW);
+  }
+  digitalWrite(obtenerPosicionLED(led1), LOW);
+  digitalWrite(obtenerPosicionLED(led2), LOW);
+  digitalWrite(obtenerPosicionLED(led3), LOW);
+  digitalWrite(obtenerPosicionLED(led1), HIGH);
+  delay(500);
+  digitalWrite(obtenerPosicionLED(led2), HIGH);
+  delay(500);
+  digitalWrite(obtenerPosicionLED(led3), HIGH);
+  delay(500);
+  for (i = 0; i < 3; i ++)
+  {
+    digitalWrite(obtenerPosicionLED(led1), LOW);
+    digitalWrite(obtenerPosicionLED(led2), LOW);
+    digitalWrite(obtenerPosicionLED(led3), LOW);
+    delay(100);
+    digitalWrite(obtenerPosicionLED(led1), HIGH);
+    digitalWrite(obtenerPosicionLED(led2), HIGH);
+    digitalWrite(obtenerPosicionLED(led3), HIGH);
+    delay(500);
+  }
+
+}
+
 void ganar (int led1, int led2, int led3) {
   cambiarJugador();
-  for(int led = 0; led <= 8; led++) {
-    cambiarEstadoLED(led, LOW);
+   for(int i = 0; i <= 8; i++) {
+    ponerValorTablero(i, 0);
   }
   ponerValorTablero(led1, jugador);
   ponerValorTablero(led2, jugador);
   ponerValorTablero(led3, jugador);
+  //Animacion de ganar
+  encenderLEDS();
+  delay(1000);
+  animacion(led1,led2,led3);
+  Serial.println("reiniciar");
+  reiniciar();
 }
+
+void alguienGana ()
+{
+  for (int i = 0; i <= 2; i++)
+  {
+    //Horizontal
+    if (obtenerValorTablero(3*i) == obtenerValorTablero(3*i+1) &&
+        obtenerValorTablero(3*i) == obtenerValorTablero(3*i+2) &&
+        obtenerValorTablero(3*i) != 0)
+    {
+       ganar(3 * i, 3 * (i+1), 3 * (i+2));
+       break;
+    }
+    //Vertical
+    else if (obtenerValorTablero(i) == obtenerValorTablero(i+3) &&
+             obtenerValorTablero(i) == obtenerValorTablero(i+6) &&
+             obtenerValorTablero(i) != 0)
+    {
+      ganar(i, i + 3, i + 6);
+      break;
+    }
+  }
+  //Diagonal esquina superior izquierda
+  if (obtenerValorTablero(0) == obtenerValorTablero(4) &&
+      obtenerValorTablero(0) == obtenerValorTablero(8) &&
+      obtenerValorTablero(0) != 0)
+  {
+    ganar(0,4,8);
+  }
+  else if (obtenerValorTablero(2) == obtenerValorTablero(4) &&
+           obtenerValorTablero(2) == obtenerValorTablero(6) &&
+           obtenerValorTablero(2) != 0)
+  {
+     ganar(2,4,6);
+  }
+  
+}
+
 bool puedeCambiarPosicion ()
 {
   for (int i = 0; i < 9; i ++)
@@ -94,6 +202,7 @@ int cambiarPosicion (int actual){
   return posicion;
 }
 
+
 void setup()
 {
   for(int led = 0; led < 9; led++) {
@@ -114,26 +223,7 @@ void loop()
       cicloJugador = !cicloJugador;
       temporizadorJugador = millis();
   }
-  for(int led = 0; led <= 8; led++) 
-  {
-    if (obtenerValorTablero(led) == 0)
-    {
-      digitalWrite(obtenerPosicionLED(led), LOW);
-    }
-    else if (obtenerValorTablero(led) == 1)
-    {
-      digitalWrite(obtenerPosicionLED(led), HIGH);
-    }
-    else if (obtenerValorTablero(led) == 2)
-    {
-      digitalWrite(obtenerPosicionLED(led), cicloJugador);
-    }
-    else if (obtenerValorTablero(led) == 3)
-    {
-      digitalWrite(obtenerPosicionLED(led), cicloSeleccion);
-    }
-    
-  }
+  encenderLEDS();
   valorBotonMover = digitalRead(botonMover);
   valorBotonSeleccion = digitalRead(botonSeleccion);
   delay(200);
@@ -159,6 +249,11 @@ void loop()
   if (obtenerValorTablero(posicionSeleccionada) == 0)
   {
     ponerValorTablero(posicionSeleccionada, 3);
+  }
+  alguienGana();
+  if (!puedeCambiarPosicion())
+  {
+    reiniciar();
   }
  
 }
